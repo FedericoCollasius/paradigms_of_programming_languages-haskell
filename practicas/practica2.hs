d78da35d27a4773e205e3116bc984614b76f973c
FedericoCollasius <fede.collasius@gmail.com>
2025-09-28 20:22:10 -0300

    did some more haskell in p2

--- practicas/practica2.hs
+++ practicas/practica2.hs
@@ -10,19 +10,31 @@ mejorSegun f = foldr1 (\ x best -> if f x best then x else best)
 elem :: Eq a => a -> [a] -> Bool
 elem e = foldr (\x acc -> e == x || acc) False 
 
-takeRecursionExplicita :: Int -> [a] -> [a]
-takeRecursionExplicita _ [] = []
-takeRecursionExplicta n (x:xs) = if n == 0 then [] else x : takeRecursionExplicita (n-1) xs
+takeRecursionExplicita :: [a] -> Int -> [a]
+takeRecursionExplicita [] _ = []
+takeRecursionExplicita (x:xs) n = if n == 0 then [] else x : (takeRecursionExplicita xs (n-1))
 
--- foldr :: (a -> b -> b) -> [a] -> [b]
+-- foldr :: (a -> b -> b) -> b -> [a] -> [b]
+-- foldr cVacio cBin l = case l of
+--                          []      -> cVacio
+--                          (x: xs) -> cBin x (rec xs) 
+--      where rec = foldr cVacio cBin
+
+
 -- f :: (a -> (Int -> [a]) -> (Int -> [a]))
-
 takeFoldr :: [a] -> Int -> [a]
-takeFoldr = foldr (\x rec -> \n -> if n == 0 then [] else x : rec(n-1)) (const [])
+takeFoldr = foldr
+        (\x rec n -> if n == 0 then [] else x : (rec (n-1)))
+        (const [])
 
 sacarUnaExplicita :: Eq a => a -> [a] -> [a]
-sacarUnaExplicita e [] = [] 
+sacarUnaExplicita _ [] = [] 
 sacarUnaExplicita e (x: xs) = if x == e then xs else sacarUnaExplicita e xs
 
-sacarUnaFoldr :: Eq a => a -> [a] -> [a]
-sacarUnaFoldr e = foldr (\x rec -> if x == e then rec else) (const [])
+data Arbol a = Hoja a | Bin (Arbol a) a (Arbol a)
+
+foldAB f g (Hoja n) = f n
+foldAB f g (Bin t1 n t2) = g (foldAB f g t1) n (foldAB f g t2)
+
+ramas :: Arbol a -> [[a]]
+ramas = foldAB (\h -> [[h]]) (\i r d -> (map (r:) i) ++ (map (r:) d))
